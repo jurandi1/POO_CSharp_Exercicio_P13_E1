d385c3e33b02c05ecea5ff625f49371ec13cfaba
Jurandi Junior <jurandijr20@gmail.com>
2025-02-01 13:18:00 -0300

    Exercício concluido

---
+++ Entities/Exceptions/DomainException.cs
@@ -0,0 +1,9 @@
+﻿namespace POO_CSharp_Exercicio_P13_E1.Entities.Exceptions
+{
+    internal class DomainException : ApplicationException
+    {
+        public DomainException(string message) : base(message)
+        {
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,9 +1,44 @@
-﻿namespace POO_CSharp_Exercicio_P13_E1
+﻿using System.Globalization;
+using System.Reflection.Metadata;
+using POO_CSharp_Exercicio_P13_E1.Entities;
+using POO_CSharp_Exercicio_P13_E1.Entities.Exceptions;
+
+namespace POO_CSharp_Exercicio_P13_E1
 {
-        internal class Program
+    internal class Program
     {
         private static void Main(string[] args)
         {
+            Console.WriteLine("__________________________________");
+            Console.WriteLine("Enter account data");
+            Console.Write("Number: ");
+            int number = int.Parse(Console.ReadLine());
+            Console.Write("Holder: ");
+            String holder = Console.ReadLine();
+            Console.Write("Initial Balance: ");
+            double balance = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            Console.Write("Withdrawn limit: ");
+            double  withdrawnLimit = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Account account = new Account(number, holder, balance, withdrawnLimit);
+
+            Console.WriteLine();
+            Console.Write("Enter amount for withdraw: ");
+            double amount = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+            try
+            {
+                account.Withdraw(amount);
+                Console.WriteLine("New Balance: " + account.Balance.ToString("F2", CultureInfo.InvariantCulture));
+            }
+            catch (DomainException e)
+            {
+                Console.WriteLine("Withdraw error: " + e.Message);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected error: " + e.Message);
+            }
+
         }
     }
 }
